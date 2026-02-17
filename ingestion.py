ac81da60a3eba759263118e251eea236fa07fbfd
Javicilie0 <183185473+Javicilie0@users.noreply.github.com.>
2026-02-17 12:57:31 +0200

    Loading error fix

--- ingestion.py
+++ ingestion.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from langchain_community.document_loaders import TextLoader, PyPDFLoader
+from langchain_community.document_loaders import TextLoader, PyPDFLoader,Docx2txtLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
 from langchain_pinecone import PineconeVectorStore
@@ -10,4 +10,15 @@ from langchain_pinecone import PineconeVectorStore
 load_dotenv()
 
 if __name__ == "__main__":
-    loader 
+    pdfLoader = PyPDFLoader("data/Rag_.pdf")
+    docLoader = Docx2txtLoader("data/LLM.docx")
+    txtLoader = TextLoader("data/MachineLearning.txt")
+
+    documents = pdfLoader.load() + docLoader.load() + txtLoader.load()
+
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    text = text_splitter.split_documents(documents)
+
+    embeddings = OllamaEmbeddings(model= "nomic-embed-text")
+
+    PineconeVectorStore.from_documents(text, embeddings, index_name=os.getenv("INDEX_NAME"))
